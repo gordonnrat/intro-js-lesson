eaddcb2ace89902d681405dcb0aeb832e014957e
gordonnrat <gordonnrat@gmail.com>
2023-03-30 17:54:06 -0700

    lesson 5 complete

--- lecture.js
+++ lecture.js
@@ -9,12 +9,32 @@ Make sure to code-along during lecture.
 // 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
 
 // 3. Creating Variables + Data Types
-    let text = "this is a sentence";
+    // let text = "this is a sentence";
 
-    // alert(text);
-    console.log(text);
+    // // alert(text);
+    // console.log(text);
 
 // 4. Creating Functions (+ Event Listeners?)
+
+
+    // function nameOfFunction() {
+    //     console.log("hello world");
+    // }
+
+    // nameOfFunction();
+
+    // function addition(a, b) {
+    //     console.log(a + b);
+    // }
+
+    // addition(5, 6)
+
+    // let add = function addition2(a, b) {
+    //     let sum = a + b;
+    //     console.log(sum)
+    // }
+
+// add(5,8);
 
 // 5. Strings
 
--- student.js
+++ student.js
@@ -19,7 +19,37 @@ Complete the following numbered tasks:
     // 4. Assign one to the type String below:
     name = "John"; 
 
-console.log("Your name is " + name + " and you are " + age + " years old.")
-console.log("You fliped a coin and it is " + heads + " that it landed on heads.")
+// console.log("Your name is " + name + " and you are " + age + " years old.")
+// console.log("You flipped a coin and it is " + heads + " that it landed on heads.")
 
-     
+/*
+
+========== LESSON 2 - FUNCTIONS ==========
+
+*/
+
+// 1. Create a doubleNumber function statement that takes in 1 number parameter and returns double the value.
+
+function doubleNumber(a) {
+    let double = 2 * a
+    console.log(double);
+    return double;
+}
+
+doubleNumber(10);
+
+// 2. Create a squareNumber function expression that takes in 1 number parameter and returns the square
+
+function squareNumber(a) {
+    let square = a**2
+    console.log(square);
+    return square;
+}
+
+squareNumber(5);
+
+// 3. Create a productNumber arrow function that takes in 2 number parameters and returns the product of the 2 numbers
+
+let productNumber = (a, b) => console.log(a * b);
+
+productNumber(2,5)
